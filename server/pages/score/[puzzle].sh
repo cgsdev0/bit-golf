8aac7e7d0adfb392f9a6fc6898156b4f1cc37af0
Sarah Schulte <git@sarah.engineer>
2023-10-01 16:05:03 -0700

    store IPs with scores

--- server/pages/score/[puzzle].sh
+++ server/pages/score/[puzzle].sh
@@ -9,8 +9,8 @@ PUZZLE=$(echo "${PATH_VARS[puzzle]}" | tr -d '\n' | sed 's/[^0-9]//g')
 if [[ "$REQUEST_METHOD" == "POST" ]]; then
   touch data/$PUZZLE
   NEW_SCORE=$(echo "${QUERY_PARAMS[score]}" | tr -d '\n' | sed 's/[^0-9]//g')
-  echo "$NEW_SCORE" >> data/$PUZZLE
+  echo "$NEW_SCORE ${HTTP_HEADERS[cf-connecting-ip]}" >> data/$PUZZLE
 fi
 
 set -o pipefail
-sort -n data/$PUZZLE | head -n 1 || echo NaN
+sort -n data/$PUZZLE | head -n 1 | cut -d' ' -f1 || echo NaN
