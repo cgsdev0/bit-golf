11bf1d6f3155d99211af229d0d2eb2c412fde3ea
Sarah Schulte <git@sarah.engineer>
2023-10-01 02:49:29 -0700

    bugfix

--- server/pages/score/[puzzle].sh
+++ server/pages/score/[puzzle].sh
@@ -3,7 +3,8 @@ PUZZLE=$(echo "${PATH_VARS[puzzle]}" | tr -d '\n' | sed 's/[^0-9]//g')
 
 if [[ "$REQUEST_METHOD" == "POST" ]]; then
   touch data/$PUZZLE
-  echo "${QUERY_PARAMS[score]}" | tr -d '\n' | sed 's/[^0-9]//g' >> data/$PUZZLE
+  NEW_SCORE=$(echo "${QUERY_PARAMS[score]}" | tr -d '\n' | sed 's/[^0-9]//g')
+  echo "$NEW_SCORE" >> data/$PUZZLE
 fi
 
 set -o pipefail
