c6747f885103e46c6746d51c2a04e6286e65a466
Sarah Schulte <git@sarah.engineer>
2023-10-04 07:08:17 -0700

    add verification

--- server/pages/score/[puzzle].sh
+++ server/pages/score/[puzzle].sh
@@ -9,7 +9,7 @@ PUZZLE=$(echo "${PATH_VARS[puzzle]}" | tr -d '\n' | sed 's/[^0-9]//g')
 if [[ "$REQUEST_METHOD" == "POST" ]]; then
   touch data/$PUZZLE
   NEW_SCORE=$(echo "${QUERY_PARAMS[score]}" | tr -d '\n' | sed 's/[^0-9]//g')
-  echo "$NEW_SCORE ${HTTP_HEADERS[cf-connecting-ip]}" >> data/$PUZZLE
+  echo "$NEW_SCORE ${HTTP_HEADERS[cf-connecting-ip]} $REQUEST_BODY" >> data/$PUZZLE
 fi
 
 set -o pipefail
