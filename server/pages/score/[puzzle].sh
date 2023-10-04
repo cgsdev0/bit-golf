2d7e6fe746ec8f01b4462389885a935cffe8672f
Sarah Schulte <git@sarah.engineer>
2023-10-04 07:19:15 -0700

    require verification

--- server/pages/score/[puzzle].sh
+++ server/pages/score/[puzzle].sh
@@ -7,6 +7,10 @@ end_headers
 PUZZLE=$(echo "${PATH_VARS[puzzle]}" | tr -d '\n' | sed 's/[^0-9]//g')
 
 if [[ "$REQUEST_METHOD" == "POST" ]]; then
+  if [[ -z "$REQUEST_BODY" ]]; then
+    echo "invalid"
+    return $(status_code 400)
+  fi
   touch data/$PUZZLE
   NEW_SCORE=$(echo "${QUERY_PARAMS[score]}" | tr -d '\n' | sed 's/[^0-9]//g')
   echo "$NEW_SCORE ${HTTP_HEADERS[cf-connecting-ip]} $REQUEST_BODY" >> data/$PUZZLE
