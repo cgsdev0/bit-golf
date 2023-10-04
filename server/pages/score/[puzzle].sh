deae44daff43c387e3d5c8842236ecabba5d9cff
Sarah Schulte <git@sarah.engineer>
2023-10-04 07:30:36 -0700

    sigh

--- server/pages/score/[puzzle].sh
+++ server/pages/score/[puzzle].sh
@@ -5,16 +5,18 @@ header Access-Control-Allow-Origin '*'
 end_headers
 
 PUZZLE=$(echo "${PATH_VARS[puzzle]}" | tr -d '\n' | sed 's/[^0-9]//g')
+VERIFICATION=$(echo "${PATH_VARS[verification]}" | tr -d '\n')
 
 if [[ "$REQUEST_METHOD" == "POST" ]]; then
-  if [[ -z "$REQUEST_BODY" ]]; then
+  if [[ -z "$VERIFICATION" ]]; then
     echo "invalid"
     return $(status_code 400)
   fi
   touch data/$PUZZLE
   NEW_SCORE=$(echo "${QUERY_PARAMS[score]}" | tr -d '\n' | sed 's/[^0-9]//g')
-  echo "$NEW_SCORE ${HTTP_HEADERS[cf-connecting-ip]} $REQUEST_BODY" >> data/$PUZZLE
+  echo "$NEW_SCORE ${HTTP_HEADERS[cf-connecting-ip]} $VERIFICATION" >> data/$PUZZLE
 fi
 
 set -o pipefail
 sort -n data/$PUZZLE | head -n 1 | cut -d' ' -f1
+return $(status_code 200)
