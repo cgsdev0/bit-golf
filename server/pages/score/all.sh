847f1115ee462874d8d27c074d9ed4d43042ef02
Sarah Schulte <git@sarah.engineer>
2023-10-07 02:46:57 -0700

    oopsy

--- server/pages/score/all.sh
+++ server/pages/score/all.sh
@@ -1,8 +1,7 @@
 # headers
 
 BS="[$(for PUZZLE in {0..14}; do
-  set -o pipefail
-  sort -n data/default/$PUZZLE | head -n 1 | cut -d' ' -f1 || echo -1
+  sort -n data/default/$PUZZLE | head -n 1 | cut -d' ' -f1
 done | paste -sd',')]"
 
 header Content-Type 'application/json'
